593c8b547f2321bbf77e63a9f72002347bd826aa
dqh-au <david.q.hogan@gmail.com>
2023-05-20 10:55:38 +1000

    Simplified client runtime.

--- runtime/client/jsx.mjs
+++ runtime/client/jsx.mjs
@@ -1,103 +1,18 @@
-let jsxDocument;
+//
+// Wrap Element.appendChild so that it can add an array of elements.
+// This allows a JSX fragment to be passed to appendChild.
+//
 
-if (typeof window === 'object')
-{
-    jsxDocument = document;
-    
-    //
-    // Wrap Element.appendChild so that it can add an array of elements.
-    // This allows a JSX fragment to be passed to appendChild.
-    //
-
-    const originalAppendChild = Element.prototype.appendChild;
-    Element.prototype.appendChild =
-        function(child)
-        {
-            const boundAppendChild = originalAppendChild.bind(this);
-            if (Array.isArray(child))
-                child.forEach(boundAppendChild);
-            else
-                boundAppendChild(child);
-        }
-}
-
-let contexts = [{}];
-
-/**
- * Obtain current context data provided by parent tags.
- */
-export function getContext()
-{
-    return contexts[contexts.length - 1];
-}
-
-/**
- * Add data to context made available by parent tags
- * @param {object} context
- */
-export function addContext(contextToAdd)
-{
-    contexts.push(Object.assign({}, contexts[contexts.length - 1], contextToAdd));
-}
-
-/**
- * Provide context to child tags, hide parent conact.
- * @param {object} context
- */
-export function setNewContext(context)
-{
-    contexts.push(context);
-}
-
-/**
- * Create a restore point that can be used to reset context to the current state
- * @param {object} context
- */
-export function createContextRestorePoint()
-{
-    return contexts.length;
-}
-
-/**
- * Remove all contexts added since the restore point was created
- * @param {object} context
- */
-export function restoreContext(restorePoint)
-{
-    if (restorePoint < 1)
-        return;
-    
-    contexts = contexts.slice(0, restorePoint);
-}
-
-export function __nakedjsx_set_document(document)
-{
-    jsxDocument = document;
-}
-
-export function __nakedjsx_get_document(document)
-{
-    return jsxDocument;
-}
-
-export function renderNow(deferredRender)
-{
-    if (typeof deferredRender === 'function')
-        return renderNow(deferredRender());
-
-    if (typeof deferredRender === 'string')
-        return jsxDocument.createTextNode(deferredRender);
-    
-    if (Array.isArray(deferredRender))
-        return deferredRender.map(deferredRender => renderNow(deferredRender));
-    
-    return deferredRender;
-}
-
-export function __nakedjsx_create_deferred_element()
-{
-    return () => __nakedjsx_create_element(...arguments); 
-}
+const originalAppendChild = Element.prototype.appendChild;
+Element.prototype.appendChild =
+    function(child)
+    {
+        const boundAppendChild = originalAppendChild.bind(this);
+        if (Array.isArray(child))
+            child.forEach(boundAppendChild);
+        else
+            boundAppendChild(child);
+    };
 
 export function __nakedjsx_create_element(tag, props, ...children)
 {
@@ -108,25 +23,14 @@ export function __nakedjsx_create_element(tag, props, ...children)
         // Make child elements selectively placeable via {props.children}
         props.children = children;
 
-        // Allow the tag implementation to call addContext.
-        let restorePoint = createContextRestorePoint();
-        
-        try
-        {
-            return renderNow(tag(props, children));
-        }
-        finally
-        {
-            // Remove any added contexts
-            restoreContext(restorePoint);
-        }
+        return tag(props, children);
     }
 
     //
     // We're dealing with regular HTML, not a JSX component
     //
 
-    const element = jsxDocument.createElement(tag);
+    const element = document.createElement(tag);
 
     Object.entries(props).forEach(
         ([name, value]) =>
@@ -144,11 +48,6 @@ export function __nakedjsx_create_element(tag, props, ...children)
     return element;
 }
 
-export function __nakedjsx_create_deferred_fragment()
-{
-    return () => __nakedjsx_create_fragment(...arguments); 
-}
-
 export function __nakedjsx_create_fragment(props)
 {
     return props.children;
@@ -158,13 +57,11 @@ export function __nakedjsx_append_child(parent, child)
 {
     if (!child)
         return;
-
-    child = renderNow(child);
     
     if (Array.isArray(child))
         child.forEach((nestedChild) => __nakedjsx_append_child(parent, nestedChild));
     else if (typeof child === 'string')
-        parent.appendChild(jsxDocument.createTextNode(child));
+        parent.appendChild(document.createTextNode(child));
     else
         parent.appendChild(child);
 }
